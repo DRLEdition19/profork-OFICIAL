380bef14772e4376b8e4fa3ff9fe69040c0c9d38
trashbus99 <csmandrew@live.com>
2024-09-12 15:43:23 +0300

    Update installer.sh

--- ps2minus/installer.sh
+++ ps2minus/installer.sh
@@ -7,8 +7,8 @@ app=ps2minus
 #---------------------------------------------------------------------------------------------------------------------------------
 ##################################################################################################################################
 export DISPLAY=:0.0 ; cd /tmp/ ; rm "/tmp/pro-framework.sh" 2>/dev/null ; rm "/tmp/$app.sh" 2>/dev/null ;
-wget --no-check-certificate --no-cache --no-cookies -q -O "/tmp/pro-framework.sh" "https://github.com/trashbus99/profork/raw/main/main/.dep/pro-framework.sh" ; 
-wget --no-check-certificate --no-cache --no-cookies -q -O "/tmp/$app.sh" "https://github.com/trashbus99/profork/raw/main/main/$app/$app.sh" ; 
+wget --no-check-certificate --no-cache --no-cookies -q -O "/tmp/pro-framework.sh" "https://github.com/trashbus99/profork/raw/master/.dep/pro-framework.sh" ; 
+wget --no-check-certificate --no-cache --no-cookies -q -O "/tmp/$app.sh" "https://github.com/trashbus99/profork/raw/master/$app/$app.sh" ; 
 dos2unix /tmp/pro-framework.sh ; dos2unix "/tmp/$app.sh" ;  
 source /tmp/pro-framework.sh  
 bash "/tmp/$app.sh"
