ae613d2de278d3bd343c7af43db28732807d5fce
trashbus99 <csmandrew@live.com>
2024-09-12 16:10:56 +0300

    Update sublime.sh

--- sublime/sublime.sh
+++ sublime/sublime.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 ######################################################################
-# BATOCERA.PRO INSTALLER
+# PROFORK INSTALLER
 ######################################################################
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
@@ -9,8 +9,8 @@ APPNAME=SUBLIME-TEXT # for installer info
 appname=sublime # directory name in /userdata/system/pro/...
 AppName=sublime # App.AppImage name
 APPPATH=/userdata/system/pro/$appname/$appname.AppImage
-APPLINK=http://batocera.pro/app/$appname.AppImage
-ORIGIN=batocera.pro@sublimetext.com # credit & info
+APPLINK=http://PROFORK/app/$appname.AppImage
+ORIGIN=PROFORK@sublimetext.com # credit & info
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
@@ -54,8 +54,8 @@ mkdir $pro/$appname 2>/dev/null
 mkdir $pro/$appname/extra 2>/dev/null
 # --------------------------------------------------------------------
 # -- prepare dependencies for this app and the installer: 
-mkdir -p ~/pro/.dep 2>/dev/null && cd ~/pro/.dep && wget --tries=10 --no-check-certificate --no-cache --no-cookies -q -O ~/pro/.dep/dep.zip https://github.com/trashbus99/profork/raw/main/.dep/dep.zip && yes "y" | unzip -oq ~/pro/.dep/dep.zip && cd ~/
-wget --tries=10 --no-check-certificate --no-cache --no-cookies -q -O $pro/$appname/extra/icon.png https://github.com/trashbus99/profork/raw/main/$appname/extra/icon.png; chmod a+x $dep/* 2>/dev/null; cd ~/
+mkdir -p ~/pro/.dep 2>/dev/null && cd ~/pro/.dep && wget --tries=10 --no-check-certificate --no-cache --no-cookies -q -O ~/pro/.dep/dep.zip https://github.com/trashbus99/profork/raw/master/.dep/dep.zip && yes "y" | unzip -oq ~/pro/.dep/dep.zip && cd ~/
+wget --tries=10 --no-check-certificate --no-cache --no-cookies -q -O $pro/$appname/extra/icon.png https://github.com/trashbus99/profork/raw/master/$appname/extra/icon.png; chmod a+x $dep/* 2>/dev/null; cd ~/
 chmod 777 ~/pro/.dep/* && for file in /userdata/system/pro/.dep/lib*; do sudo ln -s "$file" "/usr/lib/$(basename $file)"; done
 # --------------------------------------------------------------------
 # // end of dependencies 
@@ -71,7 +71,7 @@ clear
 echo
 echo
 echo
-echo -e "${X}BATOCERA.PRO/$APPNAME INSTALLER${X}"
+echo -e "${X}PROFORK/$APPNAME INSTALLER${X}"
 echo
 echo
 echo
@@ -83,7 +83,7 @@ clear
 echo
 echo
 echo -e "${X}--------------------------------------------------------"
-echo -e "${X}BATOCERA.PRO/$APPNAME INSTALLER${X}"
+echo -e "${X}PROFORK/$APPNAME INSTALLER${X}"
 echo -e "${X}--------------------------------------------------------"
 echo
 echo
@@ -95,7 +95,7 @@ clear
 echo
 echo -e "${X}--------------------------------------------------------"
 echo -e "${X}--------------------------------------------------------"
-echo -e "${X}BATOCERA.PRO/$APPNAME INSTALLER${X}"
+echo -e "${X}PROFORK/$APPNAME INSTALLER${X}"
 echo -e "${X}--------------------------------------------------------"
 echo -e "${X}--------------------------------------------------------"
 echo
@@ -107,7 +107,7 @@ clear
 echo -e "${X}--------------------------------------------------------"
 echo -e "${X}--------------------------------------------------------"
 echo -e "${X}--------------------------------------------------------"
-echo -e "${X}BATOCERA.PRO/$APPNAME INSTALLER${X}"
+echo -e "${X}PROFORK/$APPNAME INSTALLER${X}"
 echo -e "${X}--------------------------------------------------------"
 echo -e "${X}--------------------------------------------------------"
 echo -e "${X}--------------------------------------------------------"
@@ -162,7 +162,7 @@ clear
 echo
 echo
 echo
-echo -e "${W}BATOCERA.PRO/${G}$APPNAME${W} INSTALLER ${W}"
+echo -e "${W}PROFORK/${G}$APPNAME${W} INSTALLER ${W}"
 echo
 echo
 echo
@@ -174,7 +174,7 @@ clear
 echo
 echo
 echo
-echo -e "${W}BATOCERA.PRO/${W}$APPNAME${W} INSTALLER ${W}"
+echo -e "${W}PROFORK/${W}$APPNAME${W} INSTALLER ${W}"
 echo
 echo
 echo
@@ -186,7 +186,7 @@ clear
 echo
 echo
 echo -e "${L}-----------------------------------------------------------------------"
-echo -e "${W}BATOCERA.PRO/${G}$APPNAME${W} INSTALLER ${W}"
+echo -e "${W}PROFORK/${G}$APPNAME${W} INSTALLER ${W}"
 echo -e "${L}-----------------------------------------------------------------------"
 echo
 echo
@@ -198,7 +198,7 @@ clear
 echo
 echo -e "${L}-----------------------------------------------------------------------"
 echo -e "${L}-----------------------------------------------------------------------"
-echo -e "${W}BATOCERA.PRO/${W}$APPNAME${W} INSTALLER ${W}"
+echo -e "${W}PROFORK/${W}$APPNAME${W} INSTALLER ${W}"
 echo -e "${L}-----------------------------------------------------------------------"
 echo -e "${L}-----------------------------------------------------------------------"
 echo
@@ -210,7 +210,7 @@ clear
 echo -e "${L}-----------------------------------------------------------------------"
 echo -e "${L}-----------------------------------------------------------------------"
 echo -e "${L}-----------------------------------------------------------------------"
-echo -e "${W}BATOCERA.PRO/${G}$APPNAME${W} INSTALLER ${W}"
+echo -e "${W}PROFORK/${G}$APPNAME${W} INSTALLER ${W}"
 echo -e "${L}-----------------------------------------------------------------------"
 echo -e "${L}-----------------------------------------------------------------------"
 echo -e "${L}-----------------------------------------------------------------------"
@@ -289,7 +289,7 @@ cp $launcher /userdata/roms/ports/$appname.sh 2>/dev/null
 # //
 # -- get icon for shortcut,
 icon=/userdata/system/pro/$appname/extra/icon.png
-wget -q -O $icon https://github.com/trashbus99/profork/raw/main/$appname/extra/icon.png
+wget -q -O $icon https://github.com/trashbus99/profork/raw/master/$appname/extra/icon.png
 # //
 # -- prepare f1 - applications - app shortcut, 
 shortcut=/userdata/system/pro/$appname/extra/$appname.desktop
@@ -371,5 +371,5 @@ function autostart() {
 export -f autostart
 autostart
 exit 0
-# BATOCERA.PRO INSTALLER //
+# PROFORK INSTALLER //
 ##########################
