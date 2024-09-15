55b5228189ace4c9b3f0be000a4a2c5dddf311cf
trashbus99 <csmandrew@live.com>
2024-09-15 16:57:11 +0300

    Update yttv.sh

--- youtubetv/yttv.sh
+++ youtubetv/yttv.sh
@@ -1,103 +1,322 @@
-#!/usr/bin/env bash
-# PROFORK INSTALLER WITH ANIMATIONS AND COLORS
+#!/usr/bin/env bash 
+# PROFORK INSTALLER
 ######################################################################
 ######################################################################
-#                            WELCOME TO:
-#                    THE PROFORK INSTALLER EXPERIENCE              
 ######################################################################
-
-# Define a function for animated character transitions
-function animate_step {
-  local message=$1
-  local color=$2
-  local reset='\033[0m'
-  local chars=('-' '\\' '|' '/')
-
-  echo -ne "${color}${message}${reset} "
-  for i in {1..10}; do
-    for c in "${chars[@]}"; do
-      echo -ne "${color}${c}${reset}\r"
-      sleep 0.1
-    done
-  done
-  echo -ne "${color}${message}${reset}\n"
-}
-
-# Define a function to show progress with a bar animation
-function progress_bar {
-  local total_steps=$1
-  local current_step=0
-  local bar_length=40
-  local color='\033[32m' # Green
-  local reset='\033[0m'
-
-  while [[ $current_step -le $total_steps ]]; do
-    local num_hashes=$((current_step * bar_length / total_steps))
-    local bar=$(printf '#%.0s' $(seq 1 $num_hashes))
-    local spaces=$(printf ' %.0s' $(seq 1 $((bar_length - num_hashes))))
-    
-    echo -ne "${color}[${bar}${spaces}] (${current_step}/${total_steps})${reset}\r"
-    sleep 0.1
-    ((current_step++))
-  done
-  echo
-}
-
-# Output welcome message
-clear
-animate_step "PREPARING INSTALLATION... PLEASE WAIT" '\033[36m'
-sleep 1
-clear
-
-# Define app information
-APPNAME=youtubetv
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+#--------------------------------------------------------------------- 
+#       DEFINE APP INFO >> 
+APPNAME=youtubetv 
+#APPLINK=$(curl -s https://api.github.com/repos/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases | grep linux-x64 | grep "browser_download_url" | head -n 1 | sed 's,^.*https://,https://,g' | cut -d \" -f1)
+#APPHOME="github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop" 
 APPLINK=https://github.com/trashbus99/profork/releases/download/r1/YouTubeonTV-linux-x64.zip
 APPHOME=github.com/uureel/PROFORK
-
-# Animate and confirm application info
-animate_step "DOWNLOADING $APPNAME PACKAGE..." '\033[33m'
-sleep 1
-clear
-
-# Simulate downloading with an animated progress bar
-progress_bar 50
-
-# Creating necessary directories with animations
-animate_step "SETTING UP INSTALLATION PATHS..." '\033[35m'
-mkdir -p /userdata/system/pro/$APPNAME/home 2>/dev/null
-mkdir -p /userdata/system/pro/$APPNAME/config 2>/dev/null
-mkdir -p /userdata/system/pro/$APPNAME/roms 2>/dev/null
-clear
-
-# Display fancy progress with animated colors and characters
-animate_step "CONFIGURING LAUNCH COMMANDS..." '\033[34m'
+#---------------------------------------------------------------------
+#       DEFINE LAUNCHER COMMAND >>
 COMMAND='sed -i "s,!appArgs.disableOldBuildWarning,1 == 0,g" /userdata/system/pro/youtubetv/resources/app/lib/main.js 2>/dev/null && mkdir /userdata/system/pro/'$APPNAME'/home 2>/dev/null; mkdir /userdata/system/pro/'$APPNAME'/config 2>/dev/null; mkdir /userdata/system/pro/'$APPNAME'/roms 2>/dev/null; LD_LIBRARY_PATH="/userdata/system/pro/.dep:${LD_LIBRARY_PATH}" HOME=/userdata/system/pro/'$APPNAME'/home XDG_CONFIG_HOME=/userdata/system/pro/'$APPNAME'/config QT_SCALE_FACTOR="1" GDK_SCALE="1" XDG_DATA_HOME=/userdata/system/pro/'$APPNAME'/home DISPLAY=:0.0 /userdata/system/pro/'$APPNAME'/YouTubeonTV --no-sandbox --test-type "${@}"'
-
-sleep 1
+#--------------------------------------------------------------------- 
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+# --------------------------------------------------------------------
+APPNAME="${APPNAME^^}"; ORIGIN="${APPHOME^^}"; appname=$(echo "$APPNAME" | awk '{print tolower($0)}'); AppName=$appname; APPPATH=/userdata/system/pro/$appname/$AppName.AppImage
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# show console/ssh info: 
+clear 
+echo 
+echo 
+echo 
+echo -e "${X}PREPARING $APPNAME INSTALLER . . . ${X}"
+echo 
+echo 
+echo 
+# --------------------------------------------------------------------
+# -- output colors:
+###########################
+X='\033[0m'               # 
+W='\033[0m'               # 
+#-------------------------#
+RED='\033[0m'             # 
+BLUE='\033[0m'            # 
+GREEN='\033[0m'           # 
+PURPLE='\033[0m'          # 
+DARKRED='\033[0m'         # 
+DARKBLUE='\033[0m'        # 
+DARKGREEN='\033[0m'       # 
+DARKPURPLE='\033[0m'      # 
+###########################
+# --------------------------------------------------------------------
+# -- console theme
+L=$X
+R=$X
+# --------------------------------------------------------------------
+# -- prepare paths and files for installation: 
+cd ~/
+pro=/userdata/system/pro
+mkdir $pro 2>/dev/null
+mkdir $pro/extra 2>/dev/null
+rm -rf $pro/$appname 2>/dev/null
+mkdir $pro/$appname 2>/dev/null
+mkdir $pro/$appname/extra 2>/dev/null
+# --------------------------------------------------------------------
+# -- pass launcher command as cookie for main function: 
+command=$pro/$appname/extra/command; rm $command 2>/dev/null;
+echo "$COMMAND" >> $command 2>/dev/null 
+# --------------------------------------------------------------------
+# -- prepare dependencies for this app and the installer: 
+mkdir -p ~/pro/.dep 2>/dev/null && cd ~/pro/.dep && wget --tries=10 --no-check-certificate --no-cache --no-cookies -q -O ~/pro/.dep/dep.zip https://github.com/trashbus99/profork/raw/master/.dep/dep.zip && yes "y" | unzip -oq ~/pro/.dep/dep.zip && cd ~/
+wget --tries=10 --no-check-certificate --no-cache --no-cookies -q -O $pro/$appname/extra/icon.png https://github.com/trashbus99/profork/raw/master/$appname/extra/icon.png; chmod a+x $dep/* 2>/dev/null; cd ~/
+chmod 777 ~/pro/.dep/* && for file in /userdata/system/pro/.dep/lib*; do sudo ln -s "$file" "/usr/lib/$(basename $file)"; done
+# --------------------------------------------------------------------
+# // end of dependencies 
+#
+# --------------------------------------------------------------------
+# -- run before installer:  
+#killall wget 2>/dev/null && killall $AppName 2>/dev/null && killall $AppName 2>/dev/null && killall $AppName 2>/dev/null
+# --------------------------------------------------------------------
+cols=$($dep/tput cols); rm -rf /userdata/system/pro/$appname/extra/cols
+echo $cols >> /userdata/system/pro/$appname/extra/cols
+line(){
+echo 1>/dev/null
+}
+# -- show console/ssh info: 
 clear
-
-# Simulate downloading dependencies with animations
-animate_step "DOWNLOADING DEPENDENCIES..." '\033[32m'
-mkdir -p ~/pro/.dep 2>/dev/null
-cd ~/pro/.dep && wget --tries=10 --no-check-certificate --no-cache --no-cookies -q -O ~/pro/.dep/dep.zip https://github.com/trashbus99/profork/raw/master/.dep/dep.zip && yes "y" | unzip -oq ~/pro/.dep/dep.zip
+echo
+echo
+echo
+echo -e "${X}PROFORK/$APPNAME INSTALLER${X}"
+echo
+echo
+echo
+echo
+sleep 0.33
 clear
+echo
+echo
+echo -e "${W}- - - - - - -"
+echo -e "${X}PROFORK/$APPNAME INSTALLER${X}"
+echo -e "${W}- - - - - - -"
+echo
+echo
+echo
+sleep 0.33
+clear
+echo
+echo -e "${W}- - - - - - -"
+line $cols ' '; echo
+echo -e "${X}PROFORK/$APPNAME INSTALLER${X}"
+line $cols ' '; echo
+echo -e "${W}- - - - - - -"
+echo
+echo
+sleep 0.33
+clear
+line $cols '\'; echo
+line $cols '/'; echo
+line $cols ' '; echo
+echo -e "${X}PROFORK/$APPNAME INSTALLER${X}"
+line $cols ' '; echo
+line $cols '/'; echo
+line $cols '\'; echo
+echo
+sleep 0.33
+echo -e "${X}THIS WILL INSTALL YOUTUBETV FOR BATOCERA"
+echo -e "${X}USING $ORIGIN"
+echo
+echo -e "${X}$APPNAME WILL BE AVAILABLE IN PORTS"
+echo -e "${X}AND ALSO IN THE F1->APPLICATIONS MENU"
+echo -e "${X}AND INSTALLED IN /USERDATA/SYSTEM/PRO/$APPNAME"
+echo
+echo -e "${X}FOLLOW THE BATOCERA DISPLAY"
+echo
+echo -e "${X}. . .${X}" 
+echo
+#/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+# --------------------------------------------------------------------
+#\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+# -- THIS WILL BE SHOWN ON MAIN BATOCERA DISPLAY:   
+function batocera-pro-installer {
+APPNAME="$1"
+appname="$2"
+AppName="$3"
+APPPATH="$4"
+APPLINK="$5"
+ORIGIN="$6"
+# --------------------------------------------------------------------
+# -- colors: 
+###########################
+X='\033[0m'               # 
+W='\033[0m'               # 
+#-------------------------#
+RED='\033[0m'             # 
+BLUE='\033[0m'            # 
+GREEN='\033[0m'           # 
+PURPLE='\033[0m'          # 
+DARKRED='\033[0m'         # 
+DARKBLUE='\033[0m'        # 
+DARKGREEN='\033[0m'       # 
+DARKPURPLE='\033[0m'      # 
+###########################
+# -- display theme:
+L=$W
+T=$W
+R=$RED
+B=$BLUE
+G=$GREEN
+P=$PURPLE
+# --------------------------------------------------------------------
+cols=$(cat /userdata/system/pro/.dep/display.cfg | tail -n 1)
+cols=$(bc <<<"scale=0;$cols/1.3") 2>/dev/null
+#cols=$(cat /userdata/system/pro/$appname/extra/cols | tail -n 1)
+line(){
+echo 1>/dev/null
+}
+clear
+echo
+echo
+echo
+echo -e "${W}PROFORK/${G}$APPNAME${W} INSTALLER ${W}"
+echo
+echo
+echo
+echo
+sleep 0.33
+clear
+echo
+echo
+echo
+echo -e "${W}PROFORK/${W}$APPNAME${W} INSTALLER ${W}"
+echo
+echo
+echo
+echo
+sleep 0.33
+clear
+echo
+echo
+echo -e "${W}- - - - - - -"
+echo -e "${W}PROFORK/${G}$APPNAME${W} INSTALLER ${W}"
+echo -e "${W}- - - - - - -"
+echo
+echo
+echo
+sleep 0.33
+clear
+echo
+echo -e "${W}- - - - - - -"
+echo; #echo -e "${W}- - -"
+echo -e "${W}PROFORK/${W}$APPNAME${W} INSTALLER ${W}"
+echo; #echo -e "${W}- - -"
+echo -e "${W}- - - - - - -"
+echo
+echo
+sleep 0.33
+clear
+echo -e "${W}- - - - - - -"
+echo; #echo -e "${W}- - -"
+echo; #echo -e "${W}- - -"
+echo -e "${W}PROFORK/${G}$APPNAME${W} INSTALLER ${W}"
+echo; #echo -e "${W}- - -"
+echo; #echo -e "${W}- - -"
+echo -e "${W}- - - - - - -"
+echo
+sleep 0.33
 
-# Fancy transition for unzipping and installation
-animate_step "INSTALLING APPLICATION..." '\033[31m'
-mkdir -p /userdata/system/pro/$APPNAME
-cd /userdata/system/pro/$APPNAME
-wget --tries=10 --no-check-certificate --no-cache --no-cookies -q -O $pro/$appname/extra/icon.png https://github.com/trashbus99/profork/raw/master/$appname/extra/icon.png
-
-# Step-by-step animations for creating launcher
-animate_step "SETTING UP LAUNCHER..." '\033[35m'
+clear
+echo;
+echo; #echo -e "${W}- - -"
+echo; #echo -e "${W}- - -"
+echo -e "${W}PROFORK/${G}$APPNAME${W} INSTALLER ${W}"
+echo; #echo -e "${W}- - -"
+echo; #echo -e "${W}- - -"
+echo;
+echo -e "${W}THIS WILL INSTALL YOUTUBETV FOR BATOCERA"
+echo -e "${W}USING $ORIGIN"
+echo
+echo -e "${W}$APPNAME WILL BE AVAILABLE IN PORTS"
+echo -e "${W}AND ALSO IN THE F1->APPLICATIONS MENU"
+echo -e "${W}AND INSTALLED IN /USERDATA/SYSTEM/PRO/$APPNAME"
+echo 
+# --------------------------------------------------------------------
+# -- check system before proceeding
+if [[ "$(uname -a | grep "x86_64")" != "" ]]; then 
+:
+else
+echo
+echo -e "${RED}ERROR: SYSTEM NOT SUPPORTED"
+echo -e "${RED}YOU NEED BATOCERA X86_64${X}"
+echo
+sleep 5
+exit 0
+# quit
+exit 0
+fi
+# --------------------------------------------------------------------
+# -- temp for curl download
+pro=/userdata/system/pro
+temp=$pro/$appname/extra/downloads
+rm -rf $temp 2>/dev/null 
+mkdir -p $temp 2>/dev/null
+# --------------------------------------------------------------------
+echo
+echo -e "${G}DOWNLOADING${W}"
+url=https://github.com/trashbus99/profork/raw/master/
+cd $temp
+curl --progress-bar --remote-name --location "$APPLINK"
+SIZE=$(du -sh $temp | awk '{print $1}') 2>/dev/null
+echo -e "${T}$temp  ${T}$SIZE( )  ${G}OK${W}" | sed 's/( )//g'
+echo
+echo; #echo -e "${W}- - -" 
+sleep 1.333 
+# --------------------------------------------------------------------
+echo -e "${G}INSTALLING${W}" 
+# --------------------------------------------------------------------
+mkdir youtubetv 2>/dev/null
+yes "A" | unzip -qq $temp/*.zip -d $temp/youtubetv
+cd $temp/youtubetv/*/
+mv ./* $pro/$appname/
+chmod a+x $pro/$appname/You* 2>/dev/null
+chmod a+x $pro/$appname/chrome* 2>/dev/null
+chmod a+x $pro/$appname/lib* 2>/dev/null
+cd ~/pro
+rm -rf $temp
+SIZE=$(du -sh $pro/$appname | awk '{print $1}') 2>/dev/null
+echo -e "${T}$pro/$appname  ${T}$SIZE( )  ${G}OK${W}" | sed 's/( )//g'
+# --------------------------------------------------------------------
+# -- prepare launcher to solve dependencies on each run and avoid overlay, 
 launcher=/userdata/system/pro/$appname/Launcher
-echo "#!/bin/bash" >> $launcher
-echo "$COMMAND" >> $launcher
+rm -rf $launcher
+echo '#!/bin/bash ' >> $launcher
+echo 'unclutter-remote -s' >> $launcher 
+## -- GET APP SPECIFIC LAUNCHER COMMAND: 
+######################################################################
+echo "$(cat /userdata/system/pro/$appname/extra/command)" >> $launcher
+######################################################################
+dos2unix $launcher
 chmod a+x $launcher
-clear
-
-# Setting up desktop shortcut with color transitions
-animate_step "CREATING DESKTOP SHORTCUT..." '\033[34m'
+rm /userdata/system/pro/$appname/extra/command 2>/dev/null
+# --------------------------------------------------------------------
+# -- prepare f1 - applications - app shortcut, 
 shortcut=/userdata/system/pro/$appname/extra/$appname.desktop
 rm -rf $shortcut 2>/dev/null
 echo "[Desktop Entry]" >> $shortcut
@@ -107,20 +326,88 @@ echo "Exec=/userdata/system/pro/$appname/Launcher" >> $shortcut
 echo "Terminal=false" >> $shortcut
 echo "Type=Application" >> $shortcut
 echo "Categories=Game;batocera.linux;" >> $shortcut
-clear
-
-# Final animation for installation completion
-animate_step "INSTALLATION COMPLETE!" '\033[32m'
-echo -e "\033[32m$APPNAME has been successfully installed! You can now find it in Ports and Applications menu.\033[0m"
-sleep 2
-clear
-
-# Realoading Emulationstation
-animate_step "RELOADING EMULATIONSTATION..." '\033[33m'
+echo "Name=$appname" >> $shortcut
+f1shortcut=/usr/share/applications/$appname.desktop
+dos2unix $shortcut 
+chmod a+x $shortcut 
+cp $shortcut $f1shortcut 2>/dev/null
+# --------------------------------------------------------------------
+# -- prepare Ports file, 
+port="/userdata/roms/ports/YoutubeTV.sh" 
+rm $port 2>/dev/null
+echo '#!/bin/bash ' >> $port
+echo 'killall -9 YouTubeonTV && unclutter-remote -s' >> $port
+echo '/userdata/system/pro/youtubetv/Launcher' >> $port
+dos2unix $port 
+chmod a+x $port 
+# --------------------------------------------------------------------
+# -- get padtokey profile
+rm "/userdata/roms/ports/YoutubeTV.sh.keys" 
+wget -q -O "/userdata/roms/ports/YoutubeTV.sh.keys" "https://github.com/trashbus99/profork/raw/master/youtubetv/extra/YoutubeTV.sh.keys"
+# --------------------------------------------------------------------
+# -- prepare prelauncher to avoid overlay,
+pre=/userdata/system/pro/$appname/extra/startup
+rm -rf $pre 2>/dev/null
+echo "#!/usr/bin/env bash" >> $pre
+echo "cp /userdata/system/pro/$appname/extra/$appname.desktop /usr/share/applications/ 2>/dev/null" >> $pre
+dos2unix $pre
+chmod a+x $pre
+# -- add prelauncher to custom.sh to run @ reboot
+csh=/userdata/system/custom.sh
+if [[ -e $csh ]] && [[ "$(cat $csh | grep "/userdata/system/pro/$appname/extra/startup")" = "" ]]; then
+echo -e "\n/userdata/system/pro/$appname/extra/startup" >> $csh
+fi
+if [[ -e $csh ]] && [[ "$(cat $csh | grep "/userdata/system/pro/$appname/extra/startup" | grep "#")" != "" ]]; then
+echo -e "\n/userdata/system/pro/$appname/extra/startup" >> $csh
+fi
+if [[ -e $csh ]]; then :; else
+echo -e "\n/userdata/system/pro/$appname/extra/startup" >> $csh
+fi
+dos2unix $csh
+# -- done. 
+sleep 1
+echo -e "${G}> ${W}DONE${W}"
+echo
+sleep 1
+echo; #echo -e "${W}- - -"
+echo -e "${W}> $APPNAME INSTALLED ${G}OK${W}"
+#echo -e "${W}- - -"
+sleep 3
+# reaload for ports file
 killall -9 emulationstation
-clear
-
-# Final message
-animate_step "ENJOY YOUR NEW $APPNAME INSTALLATION!" '\033[36m'
-sleep 2
-clear
+}
+export -f batocera-pro-installer 2>/dev/null
+# --------------------------------------------------------------------
+# RUN:
+# |
+  batocera-pro-installer "$APPNAME" "$appname" "$AppName" "$APPPATH" "$APPLINK" "$ORIGIN"
+# --------------------------------------------------------------------
+# PROFORK INSTALLER //
+##########################
+function autostart() {
+  csh="/userdata/system/custom.sh"
+  pcsh="/userdata/system/pro-custom.sh"
+  pro="/userdata/system/pro"
+  rm -f $pcsh
+  temp_file=$(mktemp)
+  find $pro -type f \( -path "*/extra/startup" -o -path "*/extras/startup.sh" \) > $temp_file
+  echo "#!/bin/bash" > $pcsh
+  sort $temp_file >> $pcsh
+  rm $temp_file
+  chmod a+x $pcsh
+  temp_csh=$(mktemp)
+  grep -vxFf $pcsh $csh > $temp_csh
+  mapfile -t lines < $temp_csh
+  if [[ "${lines[0]}" != "#!/bin/bash" ]]; then
+    lines=( "#!/bin/bash" "${lines[@]}" )
+  fi
+  if ! grep -Fxq "$pcsh &" $temp_csh; then
+    lines=( "${lines[0]}" "$pcsh &" "${lines[@]:1}" )
+  fi
+  printf "%s\n" "${lines[@]}" > $csh
+  chmod a+x $csh
+  rm $temp_csh
+}
+export -f autostart
+autostart
+exit 0
