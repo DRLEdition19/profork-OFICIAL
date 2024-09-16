2289b2cfe1022f449683cbd8ce7d4aa0a371eff8
trashbus99 <csmandrew@live.com>
2024-09-16 21:23:19 +0300

    Update install.sh

--- app/install.sh
+++ app/install.sh
@@ -11,7 +11,7 @@ fi
 
 # Function to display animated title
 animate_title() {
-    local text="BATOCERA PRO APP INSTALLER"
+    local text="PROFORK APP INSTALLER"
     local delay=0.1
     local length=${#text}
 
@@ -25,7 +25,7 @@ animate_title() {
 # Function to display controls
 display_controls() {
     echo 
-    echo "  This Will install Batocera Pro app Installer to Ports"
+    echo "  This Will install Profork app Installer to Ports"
     echo    
     sleep 5  # Delay for 5 seconds
 }
@@ -43,25 +43,23 @@ if [ ! -d "/userdata/system/pro" ]; then
     mkdir -p /userdata/system/pro
 fi
 
-# Download pro.sh to /userdata/system/pro
-curl -L https://github.com/trashbus99/profork/raw/main/app/pro.sh -o /userdata/system/pro/pro.sh
+
 
 # Download BatoceraPRO.sh to /userdata/roms/ports
-curl -L https://github.com/trashbus99/profork/raw/main/app/BatoceraPRO.sh -o /userdata/roms/ports/BatoceraPRO.sh
+curl -L https://github.com/trashbus99/profork/raw/master/app/Profork.sh -o /userdata/roms/ports/Profork.sh
 
 # Download BatoceraPRO.sh.keys to /userdata/roms/ports
 wget  https://github.com/trashbus99/profork/raw/main/app/bkeys.txt -P /userdata/roms/ports/
 
 # Set execute permissions for the downloaded scripts
-chmod +x /userdata/system/pro/pro.sh
-chmod +x /userdata/roms/ports/BatoceraPRO.sh
+chmod +x /userdata/roms/ports/Profork.sh
 
 
 killall -9 emulationstation
 
 sleep 1
 
-mv /userdata/roms/ports/bkeys.txt /userdata/roms/ports/BatoceraPRO.sh.keys
+mv /userdata/roms/ports/bkeys.txt /userdata/roms/ports/Profork.sh.keys
 
 
 echo "Finished.  You should see BatoceraPRO in Ports"
