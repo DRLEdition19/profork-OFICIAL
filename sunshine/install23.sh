7c77fcba18b964e813f1011be71b80617e4f6b30
trashbus99 <csmandrew@live.com>
2024-09-17 18:17:26 +0300

    Update install23.sh

--- sunshine/install23.sh
+++ sunshine/install23.sh
@@ -26,7 +26,7 @@ app=sunshine
 ##################################################################################################################################
 export DISPLAY=:0.0 ; cd /tmp/ ; rm "/tmp/pro-framework.sh" 2>/dev/null ; rm "/tmp/$app.sh" 2>/dev/null ;
 wget --no-cache -q -O "/tmp/pro-framework.sh" "https://github.com/trashbus99/profork/raw/master/.dep/pro-framework.sh" ; 
-wget --no-cache -q -O "/tmp/$app.sh" "https://github.com/trashbus99/profork/raw/master/sunsine/sunshine23.sh" ; 
+wget --no-cache -q -O "/tmp/$app.sh" "https://github.com/trashbus99/profork/raw/master/sunshine/sunshine23.sh" ; 
 dos2unix /tmp/pro-framework.sh ; dos2unix "/tmp/$app.sh" ;  
 source /tmp/pro-framework.sh  
 bash "/tmp/$app.sh"
