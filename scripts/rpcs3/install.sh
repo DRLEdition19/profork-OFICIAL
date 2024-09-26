4c2f3bc08d5a5f9a6f0eeb2ac7694ab91d7cbb31
trashbus99 <csmandrew@live.com>
2024-09-26 12:20:53 +0300

    Create install.sh

---
+++ scripts/rpcs3/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Step 1: Announce the program
+echo "Profork PS3 PSN game parser"
+echo "Thanks to uureel"
+sleep 4
+clear
+
+
+# Step 2: Download the file and announce the download
+echo "Downloading batocera-rpcs3-sync script to /userdata/system..."
+wget https://raw.githubusercontent.com/trashbus99/profork/master/scripts/rpcs3/batocera-rpcs3-sync -O ~/batocera-rpcs3-sync
+# Step 3: Make the script executable
+chmod +x ~/batocera-rpcs3-sync
+echo "The script has been downloaded and made executable."
+
+# Step 4: Inform the user about how to run the script
+clear
+echo "You can run ./batocera-rpcs3-sync in /userdata/system anytime after installing pkg files to parse PS3 PSN games."
+
+sleep 5
+exit
