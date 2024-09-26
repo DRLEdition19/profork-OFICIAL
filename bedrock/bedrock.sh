9d9e9cdc2391496e71d5826e27674a2bc197a089
trashbus99 <csmandrew@live.com>
2024-09-26 15:38:35 +0300

    Update bedrock.sh

--- bedrock/bedrock.sh
+++ bedrock/bedrock.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Define the dialog box with increased dimensions
-dialog --title "WARNING!" \
-       --yesno "1. This game is unstable in Batocera. Using the Keyboard except to sign in causes crashes.\
-                \n  ONLY USE a GAMEPAD!--Limited Testing with a gamepad seems to work. \
+dialog --title "Bedrock Launcher" \
+       --yesno "1. This games now works properly via the Arch container. \
+                \n The appimage version is removed since keyboard support is broken in Batocera standalone \
                 \n2. This game requires you to own Minecraft Bedrock Edition for Android on your Google account.\
-                \n\nCONTINUE INSTALLING?" 15 70
+                \n\nCONTINUE TO ARCH CONTAINER INSTALLER?" 15 70
 
 # Capture the exit status of dialog command
 response=$?
@@ -13,7 +13,7 @@ response=$?
 # Check the response
 if [ $response -eq 0 ]; then
     # User selected Yes, execute the curl command
-  curl -L https://github.com/trashbus99/profork/raw/master/bedrock/br.sh | bash
+  curl -L https://github.com/trashbus99/profork/raw/master/steam/steam.sh | bash
 else
     # User selected No, exit the script
     exit
