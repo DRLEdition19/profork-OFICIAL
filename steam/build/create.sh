c62e96e8c6e98bd21e2ca31e08a9b251fc47b7fc
trashbus99 <csmandrew@live.com>
2024-09-27 14:51:43 +0300

    Update create.sh

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -51,19 +51,17 @@ minigalaxy legendary gamescope playonlinux minizip flatpak libreoffice \
 ripgrep i7z sd bandwhich tre zoxide p7zip atop iftop sysstat totem feh krename \
 bottles bauh flatseal rebuild-detector ccache axel breeze xorg-xdpyinfo dua-cli \
 handbrake tigervnc remmina  kitty terminator xorg-xkill media-downloader file  \
-docker docker-compose portainer-bin unzip gthumb doublecmd-qt6 dolphin nmon thunar nemo \
-gdk-pixbuf-xlib gdk-pixbuf2 xarchiver mc vifm fd krusader  krename \
-steam-boilr-gui btrfs-assistant protontricks-git lib32-sdl12-compat sdl12-compat appimagepool-appimage \
-kate kmod pciutils xrdp x11vnc tigervnc onboard remmina vinagre freerdp sunshine btrfs-progs tre plasma kde-applications \
+docker docker-compose portainer-bin unzip gthumb nmon thunar nemo \
+gdk-pixbuf-xlib gdk-pixbuf2 xarchiver mc vifm fd  \
+steam-boilr-gui btrfs-assistant protontricks-git lib32-sdl12-compat sdl12-compat appimagepool-appimage \ kmod pciutils xrdp x11vnc tigervnc onboard remmina vinagre freerdp sunshine btrfs-progs tre \
 podman distrobox cheese filezilla dos2unix wmctrl xorg-xprop fzf scc yarn sdl2 sdl2_image squashfs-tools \
 btrfs-heatmap meld lynx yq xorg xorg-server-xvfb nodejs npm cairo-dock imagemagick strace sdl2_mixer python-pysdl2 \
 tint2 plank lxde mate mate-extra dialog xterm compsize antimicrox qdirstat lutris-git chiaki procs sdl2_ttf \
 protontricks-git chiaki sublime-text-4 fuse2 heroic-games-launcher-bin moonlight-qt zoom ventoy-bin 7-zip crun runc \
 microsoft-edge-stable-bin qdirstat peazip jq steam-rom-manager-git google-chrome steamtinkerlaunch xfwm4 xfwm4-themes \
-screenfetch glances konsole discord pcem jre8-openjdk gcc13 python-pip xfce4 xfce4-goodies lua53"
-
+screenfetch glances discord jre8-openjdk gcc13 python-pip xfce4 xfce4-goodies lua53"
 # If you want to install AUR packages, specify them in this variable
-export aur_packagelist="geforcenow-electron mcpelauncher-appimage protonup-qt freefilesync-bin sgdboop-bin winegui-bin"
+export aur_packagelist="geforcenow-electron mcpelauncher-appimage pcem protonup-qt freefilesync-bin sgdboop-bin winegui-bin"
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
