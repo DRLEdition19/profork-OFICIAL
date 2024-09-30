b56508f7a6997611f2a1df4b54e16468fd266f7f
trashbus99 <csmandrew@live.com>
2024-10-01 00:25:53 +0300

    Update main.sh

--- app/main.sh
+++ app/main.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+clear
 echo "Profork"
-echo "Please Note: We do not endorse or support piracy or other illegal use." 
-sleep 3
+echo ""
+echo "This repo is provided AS-IS—no support is offered."
+echo "Disclaimer: Use for piracy or other illegal activities is not endorsed. Please use responsibly and in accordance with the law."
+sleep 4
+clear
 echo "Redirecting...."
 sleep 2
 curl -L https://raw.githubusercontent.com/trashbus99/profork/master/app/menu.sh | bash
