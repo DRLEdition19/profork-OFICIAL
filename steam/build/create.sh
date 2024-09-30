9da7ff96811b0e5373f54cf67cd8bd49e5c64a44
trashbus99 <csmandrew@live.com>
2024-09-30 10:32:36 +0300

    Update create.sh

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -52,7 +52,7 @@ ripgrep i7z sd bandwhich tre zoxide p7zip atop iftop sysstat totem feh krename \
 bottles bauh flatseal rebuild-detector ccache axel breeze xorg-xdpyinfo dua-cli \
 handbrake tigervnc remmina  kitty terminator xorg-xkill media-downloader file  \
 docker docker-compose portainer-bin unzip gthumb nmon thunar nemo \
-gdk-pixbuf-xlib gdk-pixbuf2 xarchiver mc vifm fd pcem \
+gdk-pixbuf-xlib gdk-pixbuf2 xarchiver mc vifm fd pcem deckctl steam-tweaks deck-pref-gui   \
 steam-boilr-gui btrfs-assistant protontricks-git lib32-sdl12-compat sdl12-compat appimagepool-appimage \ kmod pciutils xrdp x11vnc tigervnc onboard remmina vinagre freerdp sunshine btrfs-progs tre \
 podman distrobox cheese filezilla dos2unix wmctrl xorg-xprop fzf scc yarn sdl2 sdl2_image squashfs-tools \
 btrfs-heatmap meld lynx yq xorg xorg-server-xvfb nodejs npm cairo-dock imagemagick strace sdl2_mixer python-pysdl2 \
