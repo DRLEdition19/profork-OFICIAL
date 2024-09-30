0cb6b0b2b49c111ce6082554cc29088b579fa4f6
trashbus99 <csmandrew@live.com>
2024-10-01 00:28:07 +0300

    Update main.sh

--- app/main.sh
+++ app/main.sh
@@ -2,7 +2,10 @@
 clear
 echo "Profork"
 echo ""
+sleep 2
 echo "This repo is provided AS-IS—no support is offered."
+sleep 4
+echo ""
 echo "Disclaimer: Use for piracy or other illegal activities is not endorsed." 
 echo "Please use responsibly and in accordance with the law."
 sleep 5
