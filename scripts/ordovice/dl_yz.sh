738596e13304550626d894ff9aed995f1ee0ea32
trashbus99 <csmandrew@live.com>
2024-10-06 18:47:08 +0300

    Update dl_yz.sh

--- scripts/ordovice/dl_yz.sh
+++ scripts/ordovice/dl_yz.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 
+
+sleep 3
+echo "pulling Yuzu..."
+sleep 2
+echo ""
+echo ""
+# Download Linux-Yuzu-EA-4176.AppImage and rename to yuzuEA.AppImage, then make it executable
+wget -O "/userdata/system/switch/yuzuEA.AppImage" https://archive.org/download/yuzu_emulator_builds/yuzu/pineapple-src%20EA-4176/Linux-Yuzu-EA-4176.AppImage
+chmod +x "/userdata/system/switch/yuzuEA.AppImage"
+# Download yuzu-mainline-20240304-537296095.AppImage and rename to yuzu.AppImage, then make it executable
+wget -O "/userdata/system/switch/yuzu.AppImage" https://archive.org/download/yuzu-emulator-latest-builds-4032024/yuzu-mainline-20240304-537296095.AppImage
+chmod +x "/userdata/system/switch/yuzu.AppImage"
