0e3175ada632fb45709a71c21a40c08b57337f34
trashbus99 <csmandrew@live.com>
2024-10-06 16:50:42 +0300

    Update fb_sw.sh

--- scripts/ordovice/fb_sw.sh
+++ scripts/ordovice/fb_sw.sh
@@ -4,3 +4,32 @@ echo "If Loading freezes, ctrl-c and try again"
 curl -L bit.ly/foclabroc-switchoff | bash
 
 
+# Dialog menu prompt
+dialog --title "Batocera Version Check" \
+--yesno "Are you using Batocera version 40 or lower?\n\nSelect 'Yes' to patch the launchers.\nSelect 'No' if you are running version 41 or higher." 10 50
+
+# Capture the exit status of dialog
+response=$?
+
+if [ $response -eq 0 ]; then
+    # User selected "Yes"
+    echo "Patching launchers for version 40 or lower..."
+    
+    # Create the necessary directories if they don't exist
+    mkdir -p /userdata/system/switch/configgen
+    mkdir -p /userdata/system/switch/configgen/generators/ryujinx
+    mkdir -p /userdata/system/switch/configgen/generators/yuzu
+
+    # Download the files to the specified directories
+    curl -L https://github.com/trashbus99/profork/blob/master/scripts/ordovice/configgen/switchlauncher.py -o /userdata/system/switch/configgen/switchlauncher.py
+    curl -L https://github.com/trashbus99/profork/blob/master/scripts/ordovice/configgen/generators/ryujinx/ryujinxMainlineGenerator.py -o /userdata/system/switch/configgen/generators/ryujinx/ryujinxMainlineGenerator.py
+    curl -L https://github.com/trashbus99/profork/blob/master/scripts/ordovice/configgen/generators/yuzu/yuzuMainlineGenerator.py -o /userdata/system/switch/configgen/generators/yuzu/yuzuMainlineGenerator.py
+
+    echo "Files downloaded and patched successfully!"
+else
+    # User selected "No" or canceled the prompt
+    echo "No patches applied. Exiting..."
+    exit 0
+fi
+
+echo "DONE"
