4c979a3c555b94086c9bcc6c0f6ab28b492f68f2
trashbus99 <csmandrew@live.com>
2024-10-24 20:25:25 +0300

    Create install.sh

---
+++ pormaster/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /userdata/system
+wget https://github.com/PortsMaster/PortMaster-GUI/releases/download/2024.10.16-1432/Install.Full.PortMaster.sh
+chmod +x Install.Full.PortMaster.sh
+./Install.Full.PortMaster.sh
+clear
+echo "Portmaster is installed to ports. Refresh/update gamelist  It will usually launch on the second attempt."
+echo "Game compatibility varies"
+sleep 5
+echo""
+echo ""
+echo "Done.."
+
+
