ed06c0e560f9ec27ec0af66bd3dec4a90705d1f2
trashbus99 <csmandrew@live.com>
2024-10-27 11:27:16 +0200

    Update wquashfs.sh

--- app/wquashfs.sh
+++ app/wquashfs.sh
@@ -70,7 +70,7 @@ apps=(
     ["Zelda-2-enhanced-PC-remake"]="curl -Ls https://github.com/trashbus99/profork/raw/master/windows/zelda2.sh | bash"
     ["Celeste-Classic"]="curl -Ls https://github.com/trashbus99/profork/raw/master/windows/celeste.sh | bash" 
     ["Apotris"]="curl -Ls https://github.com/trashbus99/profork/raw/master/windows/apotris.sh | bash"
-   
+    ["Maldita-Castilla"]="curl -Ls  https://github.com/trashbus99/profork/releases/download/r1/maldita_castilla.wsquashfs | bash"
    
 
     # Add other apps here
