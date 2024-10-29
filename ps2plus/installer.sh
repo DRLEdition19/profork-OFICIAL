112bd3bfca93a57ddaac472630e4243a1ca72567
trashbus99 <csmandrew@live.com>
2024-10-29 15:57:41 +0200

    Update installer.sh

--- ps2plus/installer.sh
+++ ps2plus/installer.sh
@@ -18,4 +18,22 @@ wget --no-check-certificate --no-cache --no-cookies -q -O "/tmp/$app.sh" "https:
 dos2unix /tmp/pro-framework.sh ; dos2unix "/tmp/$app.sh" ;  
 source /tmp/pro-framework.sh  
 bash "/tmp/$app.sh"
+echo "patch -- downloading es_configs..."
+sleep 4
+# Define URLs and destination directory
+url1="https://github.com/trashbus99/profork/raw/master/ps2plus/extras/es_features_ps2plus.cfg"
+url2="https://github.com/trashbus99/profork/raw/master/ps2plus/extras/es_system_ps2plus.cfg"
+destination="/userdata/system/configs/emulationstation"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$destination"
+
+# Download the files
+curl -L "$url1" -o "$destination/es_features_ps2plus.cfg"
+curl -L "$url2" -o "$destination/es_system_ps2plus.cfg"
+
+# Confirm completion
+echo "Files downloaded to $destination."
+echo "Downloaded cfg files"
+sleep 3
 # batocera.pro // 
