51ac3bd16a0a3228a0e0083007789fc32f05581b
trashbus99 <csmandrew@live.com>
2025-01-05 19:59:03 +0200

    Create mainmenu.sh

---
+++ app/mainmenu.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+# Define the options
+OPTIONS=("1" "Arch Container (Steam, Heroic, Lutris & More apps)"
+         "2" "Standalone Apps (mostly appimages)"
+         "3" "Docker & Containers"
+         "4" "Tools"
+         "5" "Windows/Wine Freeware games"
+         "6" "Install Portmaster"
+         "7" "Install This Menu to Ports"              
+         "8" "Exit")
+         
+# Display the dialog and get the user choice
+CHOICE=$(dialog --clear --backtitle "Profork Main Menu" \
+                --title "Main Menu" \
+                --menu "Choose an option:" 15 75 3 \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+
+# Act based on the user choice
+case $CHOICE in
+    1)
+        echo "Arch Container..."
+        curl -Ls https://github.com/trashbus99/profork/raw/master/steam/steam.sh | bash
+        ;;
+    2)
+        echo "Apps Menu"
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/trashbus99/profork/raw/master/app/appmenu.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    3)
+        echo "Docker Menu..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/trashbus99/profork/raw/master/app/dockermenu.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    4)
+        echo "Tools Menu..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/trashbus99/profork/raw/master/app/tools.sh
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    5)  echo "Windows/Wine Freeware..."
+        curl -Ls https://github.com/trashbus99/profork/raw/master/app/wquashfs.sh | bash
+        ;;              
+    6)  echo "Portmaster Installer..."
+        curl -Ls https://github.com/trashbus99/profork/raw/master/portmaster/install.sh | bash
+        ;;
+    7)  echo "Ports Installer..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/trashbus99/profork/raw/master/app/install.sh
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    8)
+        echo "Exiting..."
+           exit
+        ;;
+    *)
+        echo "No valid option selected or cancelled. Exiting."
+        ;;
+esac
