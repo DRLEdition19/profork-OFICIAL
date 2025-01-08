0d48c1ab568880e3853888ad07230d0bbfc91527
trashbus99 <csmandrew@live.com>
2025-01-08 10:38:28 +0200

    Update Brave.sh

--- steam/shortcuts/Brave.sh
+++ steam/shortcuts/Brave.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && source /opt/env && brave --no-sandbox --test-type '"${@}"''
+  bash -c 'brave --no-sandbox --test-type '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
