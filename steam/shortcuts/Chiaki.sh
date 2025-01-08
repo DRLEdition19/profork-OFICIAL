08ac67468e2f8841f2dfe64cc94815f672ed5070
trashbus99 <csmandrew@live.com>
2025-01-08 10:38:51 +0200

    Update Chiaki.sh

--- steam/shortcuts/Chiaki.sh
+++ steam/shortcuts/Chiaki.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && source /opt/env && dbus-run-session chiaki '"${@}"''
+  bash -c 'dbus-run-session chiaki '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
