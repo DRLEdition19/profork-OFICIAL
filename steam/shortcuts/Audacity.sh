4dda4d3e429883c94121023050f73316adc94c6e
trashbus99 <csmandrew@live.com>
2025-01-08 10:36:47 +0200

    Update Audacity.sh

--- steam/shortcuts/Audacity.sh
+++ steam/shortcuts/Audacity.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && source /opt/env && dbus-run-session audacity '"${@}"''
+  bash -c 'dbus-run-session audacity '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
