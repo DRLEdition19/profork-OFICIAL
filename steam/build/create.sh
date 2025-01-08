a55aff20e94e1db568bc21e2fd91edbe1d7066f3
trashbus99 <csmandrew@live.com>
2025-01-08 10:33:29 +0200

    Update create.sh

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -47,7 +47,7 @@ steam-native-runtime gamemode  lib32-gamemode jre-openjdk lxterminal \
 mangohud thunderbird gimp audacity lib32-mangohud kodi ksysguard ark kate okular breeze breeze-gtk  \
 qt5-wayland xorg-server-xephyr openbox binutils emby-theater \
 xdotool xbindkeys gparted vlc smplayer mpv fish zsh xmlstarlet nvtop duf exa systemsettings dolphin  \
-legendary gamescope  minizip flatpak libreoffice  \
+legendary gamescope  minizip flatpak libreoffice pcem-git  \
 ripgrep i7z sd bandwhich tre zoxide p7zip atop iftop sysstat totem feh krename  \
 bottles bauh flatseal rebuild-detector ccache axel breeze xorg-xdpyinfo dua-cli  \
 handbrake tigervnc remmina  kitty terminator xorg-xkill media-downloader file  noto-fonts ttf-ubuntu-font-family ttf-droid \
