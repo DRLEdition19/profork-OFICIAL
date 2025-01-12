c2c3179323801ce7eba6903843c1281709337e67
trashbus99 <csmandrew@live.com>
2025-01-12 21:16:33 +0200

    Update Lutris.sh

--- steam/shortcuts/Lutris.sh
+++ steam/shortcuts/Lutris.sh
@@ -10,13 +10,14 @@ batocera-mouse show
           --bind /userdata/system/etc/passwd /etc/passwd \
           --bind /userdata/system/etc/group /etc/group \
           --bind /userdata/system /home/batocera \
+          --bind /var/lib/nvidia /run/nvidia
           --bind /sys/fs/cgroup /sys/fs/cgroup \
           --bind /userdata/system /home/root \
           --bind /etc/fonts /etc/fonts \
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session /opt/lutris/bin/lutris '"${@}"''
+  bash -c 'prepare && source /opt/env && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session /opt/lutris/bin/lutris '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
