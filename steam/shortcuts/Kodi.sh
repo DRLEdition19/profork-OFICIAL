5d1022c081b3ab3f5d10861ea5035868bdf5f9ba
trashbus99 <csmandrew@live.com>
2025-01-12 21:15:54 +0200

    Update Kodi.sh

--- steam/shortcuts/Kodi.sh
+++ steam/shortcuts/Kodi.sh
@@ -9,7 +9,7 @@ batocera-mouse show
           --bind /userdata/system/flatpak /var/lib/flatpak \
           --bind /userdata/system/etc/passwd /etc/passwd \
           --bind /userdata/system/etc/group /etc/group \
-          --bind /var/run/nvidia /var/run/nvidia \
+          --bind /var/run/nvidia /run/nvidia \
           --bind /userdata/system /home/batocera \
           --bind /sys/fs/cgroup /sys/fs/cgroup \
           --bind /userdata/system /home/root \
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'dbus-run-session kodi '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session kodi '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
