f04ff08453d1c270314e42e24b9c47668295c45f
trashbus99 <csmandrew@live.com>
2025-01-12 21:07:56 +0200

    Update Blender.sh

--- steam/shortcuts/Blender.sh
+++ steam/shortcuts/Blender.sh
@@ -9,7 +9,7 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /userdata/system/flatpak /var/lib/flatpak \
           --bind /userdata/system/etc/passwd /etc/passwd \
           --bind /userdata/system/etc/group /etc/group \
-          --bind /var/run/nvidia /var/run/nvidia \
+          --bind /var/run/nvidia /run/nvidia \
           --bind /userdata/system /home/batocera \
           --bind /sys/fs/cgroup /sys/fs/cgroup \
           --bind /userdata/system /home/root \
@@ -17,7 +17,7 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'dbus-run-session blender '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session blender '"${@}"''
 #------------------------------------------------
 batocera-mouse hide
 #------------------------------------------------
