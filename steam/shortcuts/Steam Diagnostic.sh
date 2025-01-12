140f69ac2ea682ed2d10a2c3bd8dd40326f9a886
trashbus99 <csmandrew@live.com>
2025-01-12 21:32:53 +0200

    Update Steam Diagnostic.sh

--- steam/shortcuts/Steam Diagnostic.sh
+++ steam/shortcuts/Steam Diagnostic.sh
@@ -11,13 +11,14 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /userdata/system/etc/passwd /etc/passwd \
           --bind /userdata/system/etc/group /etc/group \
           --bind /userdata/system /home/batocera \
+          --bind /var/run/nvidia /run/nvidia
           --bind /sys/fs/cgroup /sys/fs/cgroup \
           --bind /userdata/system /home/root \
           --bind /etc/fonts /etc/fonts \
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  xterm -fs 12 -maximized -fg '#888888' -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c 'ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && DISPLAY=:0.0 dbus-run-session steam'
+  xterm -fs 12 -maximized -fg '#888888' -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c 'prepare && source /opt/env && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && DISPLAY=:0.0 dbus-run-session steam'
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
