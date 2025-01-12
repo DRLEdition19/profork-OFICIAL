fb1f63689e9742a362d3a9ba3e6b681893bd9128
trashbus99 <csmandrew@live.com>
2025-01-12 21:20:37 +0200

    Update Steam.sh

--- steam/shortcuts/Steam.sh
+++ steam/shortcuts/Steam.sh
@@ -9,6 +9,7 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /userdata/system/containers/storage /var/lib/containers/storage \
           --bind /userdata/system/flatpak /var/lib/flatpak \
           --bind /userdata/system/etc/passwd /etc/passwd \
+          --bind /var/run/nvidia /run/nvidia
           --bind /userdata/system/etc/group /etc/group \
           --bind /userdata/system /home/batocera \
           --bind /sys/fs/cgroup /sys/fs/cgroup \
@@ -17,7 +18,7 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session steam '"${@}"''
+  bash -c 'prepare && source /opt/env && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session steam '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
