88790e74b5588b3653342c7f8f6063f0612d447e
trashbus99 <csmandrew@live.com>
2025-01-12 21:24:03 +0200

    Update Heroic Game Launcher.sh

--- steam/shortcuts/Heroic Game Launcher.sh
+++ steam/shortcuts/Heroic Game Launcher.sh
@@ -12,11 +12,12 @@ batocera-mouse show
           --bind /userdata/system /home/batocera \
           --bind /sys/fs/cgroup /sys/fs/cgroup \
           --bind /userdata/system /home/root \
+          --bind /var/run/nvidia /run/nvidia \
           --bind /etc/fonts /etc/fonts \
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session heroic --no-sandbox --test-type '"${@}"''
+  bash -c 'prepare && source /opt/env && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session heroic --no-sandbox --test-type '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
