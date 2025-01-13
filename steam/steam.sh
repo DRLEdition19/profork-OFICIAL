9e5f489a27497e6df6beffc79da06a73e149f38b
trashbus99 <csmandrew@live.com>
2025-01-13 21:09:41 +0200

    Update steam.sh

--- steam/steam.sh
+++ steam/steam.sh
@@ -42,7 +42,7 @@ case $CHOICE in
      1)
         echo "Installing Arch Container..."
         rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runnerhttps://github.com/trashbus99/profork/raw/master/steam/install2.sh
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/trashbus99/profork/raw/master/steam/install2.sh
         dos2unix /tmp/runner 2>/dev/null 
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
