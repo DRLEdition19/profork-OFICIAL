bb5cfd0039398f390ed58e1c0d0aca38569e0259
trashbus99 <csmandrew@live.com>
2025-01-16 20:39:37 +0200

    Update select.sh

--- steam/select.sh
+++ steam/select.sh
@@ -12,14 +12,35 @@ build_from_scratch() {
     curl -L https://github.com/trashbus99/profork/raw/master/steam/install_new.sh | bash
 }
 
+# Function to install Steam only mini container (AMD/INTEL GPU ONLY)
+install_steam_mini() {
+    echo "Installing Steam Only Mini Container..."
+    curl -L  https://github.com/trashbus99/profork/raw/master/steam/steam_installer.sh| bash
+}
+
+# Function to install Lutris only mini container (AMD/INTEL GPU ONLY)
+install_lutris_mini() {
+    echo "Installing Lutris Only Mini Container..."
+    curl -L  https://github.com/trashbus99/profork/raw/master/lutris/lutris.sh | bash
+}
+
+# Function to install Minecraft Bedrock Edition mini container (AMD/INTEL GPU ONLY)
+install_minecraft_mini() {
+    echo "Installing Minecraft Bedrock Edition Mini Container..."
+    curl -L https://github.com/trashbus99/profork/raw/master/bedrock/install.sh | bash
+}
+
 # Display the menu
 while true; do
     CHOICE=$(dialog --clear \
                     --backtitle "Install Menu" \
                     --title "Choose Installation Method" \
-                    --menu "Select an option:" 10 80 2 \
-                    1 "Download Prebuilt Image (Jan 8, 2025)" \
-                    2 "Build Up-to-Date Container from Scratch (30-90 minutes)" \
+                    --menu "Select an option:" 15 80 5 \
+                    1 "NVIDIA/AMD/INTEL GPUS: Download Prebuilt full container (Jan 8, 2025)" \
+                    2 "NVIDIA/AMD/INTEL GPUS: Build Up-to-Date full Container from Scratch (30-90 minutes)" \
+                    3 "AMD/INTEL GPU ONLY: Steam only mini container" \
+                    4 "AMD/INTEL GPU ONLY: Lutris Only mini container" \
+                    5 "AMD/INTEL GPU ONLY: Minecraft Bedrock Edition only mini container" \
                     3>&1 1>&2 2>&3)
 
     clear
@@ -30,6 +51,18 @@ while true; do
             ;;
         2)
             build_from_scratch
+            break
+            ;;
+        3)
+            install_steam_mini
+            break
+            ;;
+        4)
+            install_lutris_mini
+            break
+            ;;
+        5)
+            install_minecraft_mini
             break
             ;;
         *)
