df7ebaf73f819d5395358a639dcc60696a4a9036
trashbus99 <csmandrew@live.com>
2025-01-31 09:24:43 +0200

    Update 2010.sh

--- mame2010_v41+/2010.sh
+++ mame2010_v41+/2010.sh
@@ -75,5 +75,7 @@ elif ! grep -q "$PRO_CUSTOM_SH" "$CUSTOM_SH"; then
     chmod +x "$CUSTOM_SH"
 fi
 
-echo "Setup complete! MAME 0.139 is installed, and the startup script will run at boot."
+echo "Setup complete! MAME 0.139 is installed, and the mame 2010 startup script will run at next boot."
+echo "Please reboot Batocera for effects to take effect"
+sleep 8
 
