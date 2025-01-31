3e4d862d7b98bf3588ee2f81e89b98bf07c533dc
trashbus99 <csmandrew@live.com>
2025-01-31 14:06:39 +0200

    Update install.sh

--- portmaster/install.sh
+++ portmaster/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+dialog --msgbox "Portmaster will be installed to ports. It will usually launch on the second launch attempt.\n\nGame compatibility varies." 10 60
 cd /userdata/system
 rm -f Install.Full.PortMaster.sh
 wget https://github.com/PortsMaster/PortMaster-GUI/releases/download/2024.10.16-1432/Install.Full.PortMaster.sh
@@ -8,5 +9,5 @@ chmod +x Install.Full.PortMaster.sh
 rm Install.Full.PortMaster.sh
 dialog --msgbox "Portmaster is installed to ports. Refresh/update the gamelist. It will usually launch on the second launch attempt.\n\nGame compatibility varies." 10 60
 
-
+echo "Install finished - You should see a new entry in ports after rebooting. It usually launches on the second attempt"
 
