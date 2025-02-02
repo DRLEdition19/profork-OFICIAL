6e67754f5188843baeefb4555e27500d9d28eae7
trashbus99 <csmandrew@live.com>
2025-02-02 12:49:10 +0200

    Update shadps4.sh

--- shadps4/shadps4.sh
+++ shadps4/shadps4.sh
@@ -69,8 +69,6 @@ ZIP_REGEX="shadps4-linux-qt-.*\.zip"
 
 # Directories for additional resources
 ROMS_DIR="/userdata/roms/ps4"
-GAMELIST_PATH="$ROMS_DIR/gamelist.xml"      # For ES configuration and manual shortcut updates.
-PROCESSED_LIST="/userdata/system/.local/share/shadps4/processed_games.txt"
 IMAGE_DIR="$INSTALL_DIR/images"
 
 # Create required directories
