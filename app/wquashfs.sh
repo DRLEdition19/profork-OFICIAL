0491b9cd1e55ca5f69e9b2dbc925f9a6b9a2ca8d
trashbus99 <csmandrew@live.com>
2025-02-11 15:17:48 +0200

    Update wquashfs.sh

--- app/wquashfs.sh
+++ app/wquashfs.sh
@@ -74,7 +74,8 @@ apps=(
     ["Donkey-Kong-Advanced"]="curl -Ls https://github.com/trashbus99/profork/raw/master/windows/dka.sh | bash"
     ["The-Ur-Quan-Masters"]="curl -Ls https://github.com/trashbus99/profork/raw/master/windows/uqm.sh | bash"       
     ["SPACE-QUEST-3D/SQ3-REMAKE"]="curl -Ls https://github.com/trashbus99/profork/raw/master/windows/sq3d.sh | bash"
-    ["Quest-For-Glory-2-VGA"]="curl -Ls https://github.com/trashbus99/profork/raw/master/windows/qfg2.sh | bash"
+    ["Quest-For-Glory-2-VGA-REMAKE"]="curl -Ls https://github.com/trashbus99/profork/raw/master/windows/qfg2.sh | bash"
+    ["KQ1-2-3-VGA-AGDI-REMAKES"]=curl -Ls https://github.com/trashbus99/profork/raw/master/windows/kq.sh | bash"
     # Add other apps here
 )
 
