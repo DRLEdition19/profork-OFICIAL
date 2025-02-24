d847962d4c1896152052b9a42d690a71f11d9a06
Cliffy <csmandrew@live.com>
2025-02-24 19:27:27 +0200

    Update og.sh

--- opengoal/og.sh
+++ opengoal/og.sh
@@ -5,7 +5,7 @@
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
 APPNAME=OPEN-GOAL-LAUNCHER # for installer info
-appname=Opengoal # directory name in /userdata/system/pro/...
+appname=opengoal # directory name in /userdata/system/pro/...
 AppName=$appname # App.AppImage name
 APPPATH=/userdata/system/pro/$appname/$appname.appimage
 APPLINK=$(curl -s https://api.github.com/repos/open-goal/launcher/releases/latest | jq -r ".assets[] | select(.name | endswith(\".AppImage\")) | .browser_download_url")
